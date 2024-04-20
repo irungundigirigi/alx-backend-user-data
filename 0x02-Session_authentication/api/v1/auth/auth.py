0ea1e0546fac9b7eaf5b6d65a1cad1112b226c11
edwin <irungundigirigi@gmai.com>
2024-04-20 23:52:58 +0300

    session auth

--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -2,6 +2,7 @@
 """
 Definition of class Auth
 """
+import os
 from flask import request
 from typing import (
     List,
@@ -42,7 +43,7 @@ class Auth:
 
     def authorization_header(self, request=None) -> str:
         """
-        Returns the authorization header from a request
+        Returns the authorization header from a request object
         """
         if request is None:
             return None
@@ -53,6 +54,19 @@ class Auth:
 
     def current_user(self, request=None) -> TypeVar('User'):
         """
-        Returns a User instance from information from a request
+        Returns a User instance from information from a request object
         """
         return None
+
+    def session_cookie(self, request=None):
+        """
+        Returns a cookie from a request
+        Args:
+            request : request object
+        Return:
+            value of _my_session_id cookie from request object
+        """
+        if request is None:
+            return None
+        session_name = os.getenv('SESSION_NAME')
+        return request.cookies.get(session_name)
